21f559a7ee40c5637d08ef0527202b72a7f8e623
ayomide akinbinu <112719203+CODING-ALPHA@users.noreply.github.com>
2024-07-02 12:04:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  function updateTime() {
+    const utcTime = new Date().toUTCString().split(" ")[4];
+    const dayOfWeek = new Date().toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+
+    document.getElementById(
+      "currentTime"
+    ).textContent = `Current Time in UTC: ${utcTime}`;
+    document.getElementById(
+      "currentDay"
+    ).textContent = `Current Day: ${dayOfWeek}`;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
+});
